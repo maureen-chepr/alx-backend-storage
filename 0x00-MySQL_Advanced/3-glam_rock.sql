78867020ddcff3136507410daaeec2ad4fce191e
root <maureenchep95@gmail.com>
2024-05-11 21:42:20 +0300

    sql advanced

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- script that lists all bands with Glam rock
+
+SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE "%Glam rock%"
+ORDER BY lifespan DESC
